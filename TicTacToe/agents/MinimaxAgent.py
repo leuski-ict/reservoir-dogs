474adcf8896f8cf88c40d08ffdcd58171fee271f
Anton Leuski <leuski@gmail.com>
2024-08-23 16:47:06 -0700

    refactoring to expose useful methods

--- TicTacToe/agents/MinimaxAgent.py
+++ TicTacToe/agents/MinimaxAgent.py
@@ -29,9 +29,8 @@ class MinimaxAgent(AbstractAgent):
         if is_maximizing:
             max_eval = float('-inf')
             for move in game.available_actions():
-                prediction = self.evaluate_move(
-                    move, game, depth + 1, alpha, beta, player,
-                    not is_maximizing)
+                prediction = self.evaluate_move(move, game, player, depth + 1,
+                                                alpha, beta, not is_maximizing)
                 max_eval = max(max_eval, prediction)
                 alpha = max(alpha, prediction)
                 if beta <= alpha:
@@ -40,37 +39,44 @@ class MinimaxAgent(AbstractAgent):
         else:
             min_eval = float('inf')
             for move in game.available_actions():
-                prediction = self.evaluate_move(
-                    move, game, depth + 1, alpha, beta, player,
-                    not is_maximizing)
+                prediction = self.evaluate_move(move, game, player, depth + 1,
+                                                alpha, beta, not is_maximizing)
                 min_eval = min(min_eval, prediction)
                 beta = min(beta, prediction)
                 if beta <= alpha:
                     break
             return min_eval
 
-    def evaluate_move(self, move, game: Game, depth, alpha, beta,
-                      player,
-                      is_maximizing):
+    def evaluate_move(self, move, game: Game, player, depth=0,
+                      alpha=float('-inf'), beta=float('inf'),
+                      is_maximizing=False):
         game.make_move(move)
-        prediction = self.minimax(game, depth + 1, alpha, beta, player,
+        prediction = self.minimax(game, depth, alpha, beta, player,
                                   is_maximizing)
         game.take_back_move(move)
         return prediction
+
+    def all_best_move_estimations(self, game: Game, player):
+        if player is None:
+            player = game.current_player
+        key = (game.board.board_x, game.board.board_o, player)
+        if key not in MinimaxAgent._cache:
+            estimations = [
+                (move, self.evaluate_move(move, game, player))
+                for move in game.available_actions()]
+            max_estimation = max(estimations, key=lambda x: x[1])[1]
+            best_moves = [pair for pair in estimations if
+                          pair[1] == max_estimation]
+            MinimaxAgent._cache[key] = best_moves
+        return MinimaxAgent._cache[key]
+
+    def best_move_estimation(self, game: Game, player):
+        return random.choice(self.all_best_move_estimations(game, player))
 
     def best_move(self, game: Game, player):
         if player is None:
             player = game.current_player
         if self.epsilon > 0 and random.random() < self.epsilon:
             return random.choice(game.available_actions())
-        key = (game.board.board_x, game.board.board_o, player)
-        if key not in MinimaxAgent._cache:
-            estimations = [
-                (move, self.evaluate_move(move, game, 0, float('-inf'),
-                                          float('inf'), player, False))
-                for move in game.available_actions()]
-            max_estimation = max(estimations, key=lambda x: x[1])[1]
-            best_moves = [pair[0] for pair in estimations if
-                          pair[1] == max_estimation]
-            MinimaxAgent._cache[key] = best_moves
-        return random.choice(MinimaxAgent._cache[key])
+        est = self.best_move_estimation(game, player)
+        return est[0]
