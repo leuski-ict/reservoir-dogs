d4f9f214c76fe3fca15db0da80b1a2a87e8d4933
Anton Leuski <leuski@gmail.com>
2024-08-23 16:46:33 -0700

    another experimental environment.

--- TicTacToe/environments/ReservoirGameEnvironment.py
+++ TicTacToe/environments/ReservoirGameEnvironment.py
@@ -1,6 +1,8 @@
 from Game import Game
+from Board import Board
 from environments.GameEnvironment import GameEnvironment
 import numpy as np
+from scipy.stats import norm
 
 # Assume this table converts 4-bit sequences to float values
 CONDUCTANCE_TABLE = {
@@ -31,7 +33,8 @@ class MeanReservoirGameEnvironment(GameEnvironment):
     name = "Mean"
 
     def __init__(self, game: Game = Game(),
-                 sample: bool = False, parity: bool = True):
+                 sample: bool = False,
+                 parity: bool = True):
         super().__init__(game)
         self.sample = sample
         self.parity = parity
@@ -48,30 +51,35 @@ class MeanReservoirGameEnvironment(GameEnvironment):
     def max_input_value(self):
         return CONDUCTANCE_TABLE_MAX
 
+    @staticmethod
+    def value_for_row(board: Board, player, row: int, parity, sample) -> float:
+        position_bits = []
+        count = 0
+        for j in range(board.size):
+            if board[row, j] == player:
+                position_bits.append(1)
+                count += 1
+            else:
+                position_bits.append(0)
+        if parity:
+            position_bits.append(count % 2)
+        else:
+            position_bits.append(0)
+        value_and_error = CONDUCTANCE_TABLE[tuple(position_bits)]
+        if sample:
+            value = np.random.normal(
+                value_and_error[0], value_and_error[1])
+            return max(min(value, CONDUCTANCE_TABLE_MAX),
+                       CONDUCTANCE_TABLE_MIN)
+        else:
+            return value_and_error[0]
+
     def encoded_board(self, current_player):
         encoded_board = []
         for player in [current_player, -current_player]:
             for i in range(self.game.board.size):
-                position_bits = []
-                count = 0
-                for j in range(self.game.board.size):
-                    if self.game.board[i, j] == player:
-                        position_bits.append(1)
-                        count += 1
-                    else:
-                        position_bits.append(0)
-                if self.parity:
-                    position_bits.append(count % 2)
-                else:
-                    position_bits.append(0)
-                value_and_error = CONDUCTANCE_TABLE[tuple(position_bits)]
-                if self.sample:
-                    value = np.random.normal(
-                        value_and_error[0], value_and_error[1])
-                    value = max(min(value, CONDUCTANCE_TABLE_MAX),
-                                CONDUCTANCE_TABLE_MIN)
-                else:
-                    value = value_and_error[0]
+                value = MeanReservoirGameEnvironment.value_for_row(
+                    self.game.board, player, i, self.parity, self.sample)
                 encoded_board.append(value)
         return np.array(encoded_board, dtype=np.float32)
 
@@ -88,3 +96,57 @@ class SampledNoParityReservoirGameEnvironment(MeanReservoirGameEnvironment):
 
     def __init__(self, game: Game = Game()):
         super().__init__(game, sample=True, parity=False)
+
+
+class MeanNoParityReservoirGameEnvironment(MeanReservoirGameEnvironment):
+    name = "Mean_NP"
+
+    def __init__(self, game: Game = Game()):
+        super().__init__(game, sample=False, parity=False)
+
+
+class DecodingReservoirGameEnvironment(GameEnvironment):
+    name = "Decoding"
+
+    def __init__(self, game: Game = Game()):
+        super().__init__(game)
+        self.parity = True
+        self.sample = True
+
+    @property
+    def input_count(self):
+        return 12
+
+    @property
+    def min_input_value(self):
+        return -1
+
+    @property
+    def max_input_value(self):
+        return 1
+
+    distributions = {}
+
+    def encoded_board(self, current_player):
+        encoded_board = [0 for _ in range(self.input_count)]
+        for player in [current_player, -current_player]:
+            for i in range(self.game.board.size):
+                value = MeanReservoirGameEnvironment.value_for_row(
+                    self.game.board, player, i, self.parity, self.sample)
+
+                for key in CONDUCTANCE_TABLE:
+                    if key not in DecodingReservoirGameEnvironment.distributions:
+                        mean_and_error = CONDUCTANCE_TABLE[key]
+                        distribution = norm(
+                            loc=mean_and_error[0], scale=mean_and_error[1] / 2)
+                        scale = distribution.pdf(mean_and_error[0])
+                        DecodingReservoirGameEnvironment.distributions[
+                            key] = (distribution, scale)
+                    distribution_and_scale = \
+                        DecodingReservoirGameEnvironment.distributions[key]
+                    val = distribution_and_scale[0].pdf(value) / \
+                          (12 * distribution_and_scale[1])
+                    for index, bit in enumerate(key):
+                        if bit == 1:
+                            encoded_board[i * len(key) + index] += val * player
+        return np.array(encoded_board, dtype=np.float32)
